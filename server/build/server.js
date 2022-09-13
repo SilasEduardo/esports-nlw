c35ac01b1410eb736885db4849ac139b01e96b62
silas de paula <silasandrade94@gmail.com>
2022-09-13 20:57:41 -0300

    build

---
+++ server/build/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.get('/ads', (req, res) => {
+    res.status(201).json([
+        { id: 1, nome: "Silas" },
+        { id: 2, nome: "Paulo" },
+        { id: 3, nome: "Ricardo" }
+    ]);
+});
+app.listen(3333, () => {
+    console.log('serve load');
+});
